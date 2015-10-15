923695b4bfc344a7bbe3cf1e7ea84cef021cd542
dgorjup <domen_gorjup@hotmail.com>
2015-10-15 09:11:59 +0200

    Added local DFT example, attempted DC offset fix, fixed version control

--- arduino_DFT.py
+++ arduino_DFT.py
@@ -1,58 +1,9 @@
-<<<<<<< HEAD
-import matplotlib.pyplot as plt
-import numpy as np
-from numpy import fft 
-
-
-# ------------------------------------
-# DFT
-# ------------------------------------
-
-#signal
-
-wx = 50
-wy = [100,150]
-wz = 250
-
-n = 1000
-timeStep = 0.001
-
-t = np.linspace(0.0, n*timeStep, n)
-
-z = np.sin(2*np.pi*t * wz)
-x = np.sin(2*np.pi*t * wx) 
-y = np.sin(2*np.pi*t * wy[0]) + 0.5*np.sin(2*np.pi*t * wy[1])
-
-
-
-zf = fft.fft(z)/n
-zf = fft.fftshift(zf)   #shift zero f to center
-
-xf = fft.fft(x)/n
-xf = fft.fftshift(xf)
-
-yf = fft.fft(y)/n
-yf = fft.fftshift(yf)
-
-freq = fft.fftfreq(n,d=timeStep)
-freq = fft.fftshift(freq)
-
-
-
-plt.figure()
-plt.hold(True)
-plt.plot(freq, np.abs(zf)**2, 'g')
-plt.plot(freq, np.abs(xf)**2, 'b')
-plt.plot(freq, np.abs(yf)**2, 'r')
-
-plt.grid()
-=======
 import serial
 import serial.tools.list_ports
 from time import sleep
 import matplotlib.pyplot as plt
 import numpy as np
-from numpy.fft import fft as fft
+from numpy import fft 
 
 serialSpeed = 115200
 
@@ -106,32 +57,65 @@ print("Sampling ended.\n\nRESULT:\t%d samples\n\nClosing serial." %count)
 
 ser.close()
 
+
+# ------------------------------------
+# IZRAČUN POSPEŠKOV
+# ------------------------------------
+zero = 350.5    # povprečna vrednsot za (ADXL335)
+g1 = 428.2      # povprečna vrednost z osi pri merovanju (ADXL335)
+# g = meritev - zero / (g1-zero)
+
+x = (x - zero) / (g1-zero)
+y = (y - zero) / (g1-zero)
+z = (z - zero) / (g1-zero) 
+
+# remove DC offet
+xn = x - np.mean(x)
+yn = y - np.mean(y)
+zn = z - np.mean(z)
+
 # ------------------------------------
 # DFT
 # ------------------------------------
-z = np.random.randint(100,400,size=1000)
-x = np.random.randint(100,400,size=1000)
-y = np.random.randint(100,400,size=1000)
-zf = fft(z)
-xf = fft(x)
-yf = fft(y)
+
+n = t.size
 
 #calculate time step:
 steps =  np.array([], dtype=float)
 for i in range(1,t.size):
     step = t[i] - t[i-1]
     steps= np.append(steps, step)
-timeStep = np.mean(steps)/1000
-timeStep = 2/1000
+timeStep = np.mean(steps)#/1000  # /1000, ker je čas v ms
 
-n = t.size
-freq = np.fft.fftfreq(n,d=timeStep)
+zf = fft.fft(zn)/n
+zf = fft.fftshift(zf)   #shift zero f to center
+
+xf = fft.fft(xn)/n
+xf = fft.fftshift(xf)
+
+yf = fft.fft(yn)/n
+yf = fft.fftshift(yf)
+
+freq = fft.fftfreq(n,d=timeStep)
+freq = fft.fftshift(freq)
+
+
+# ------------------------------------
+# FILTER ?
+# ------------------------------------
+
 
 plt.figure()
 plt.hold(True)
-plt.plot(freq, zf, 'g')
-plt.plot(freq, xf, 'b')
-plt.plot(freq, yf, 'r')
+plt.plot(freq, np.abs((zf)**2), 'g')
+plt.plot(freq, np.abs((xf)**2), 'b')
+plt.plot(freq, np.abs((yf)**2), 'r')
 
->>>>>>> 56deff5fce1474faa0228a1014afb95fb7ab6bd7
+
+plt.figure()
+plt.hold(True)
+plt.plot(t,x,'b')
+plt.plot(t,y,'r')
+plt.plot(t,z,'g')
+
 plt.show()
